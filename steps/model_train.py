717bf4b7dd269359665fdb29ec663a7e0025fd0d
blue-samarth <samarth38work@gmail.com>
2024-12-31 00:44:41 +0530

    no

--- steps/model_train.py
+++ steps/model_train.py
@@ -3,7 +3,28 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
 
 @step
-def train_model(data: pd.DataFrame) -> None:
-    pass
+def train_model(self,
+                X_train: pd.DataFrame,
+                X_test: pd.DataFrame,
+                y_train: pd.Series,
+                y_test: pd.Series,
+                ) -> RegressorMixin:
+    """
+    Trains the model of the ingested data
+    Args:
+        X_train: pd.DataFrame: Training data
+        X_test: pd.DataFrame: Testing data
+        y_train: pd.Series: Training labels
+        y_test: pd.Series: Testing labels
+    Returns:
+        RegressorMixin: Trained model
+    """
+    try:
+        pass
+    except Exception as e:
+        logging.error(f"Error in training model: {e}")
+        raise e
