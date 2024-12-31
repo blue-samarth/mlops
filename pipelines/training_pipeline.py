a20b86c67896eed6dd8b75b6a45c07feaa7d592d
blue-samarth <samarth38work@gmail.com>
2025-01-01 02:55:06 +0530

    pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,5 +1,5 @@
 from zenml import pipeline
-from pandas import DataFrame
+import pandas as pd
 
 from steps.ingest_data import ingest_df
 from steps.clean_data import clean_df
@@ -8,7 +8,7 @@ from steps.eval_model import eval_model
 
 @pipeline()
 def train_pipeline(data_path : str) -> None:
-    df : DataFrame = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    eval_model(df)
+    df : pd.DataFrame = ingest_df(data_path)
+    X_tarin , X_test , y_train , y_test = clean_df(df)
+    model = train_model(X_tarin , y_train)
+    r2 , mse = eval_model(model , X_test , y_test)
