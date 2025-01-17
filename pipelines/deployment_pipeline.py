702b65110d20aa6d65fcaa0ac0d11e50b7a74bd2
blue-samarth <samarth38work@gmail.com>
2025-01-18 03:29:45 +0530

    error removal

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -1,14 +1,12 @@
-# import numpy as np
 import pandas as pd
+from pydantic import BaseModel
 
 from zenml import pipeline , step  
-from zenml.steps.base_steps import BaseParameters #, Output
 from zenml.config import DockerSettings
 from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
 from zenml.integrations.constants import MLFLOW
 
-# from zenml.integrations.mlflow.model_deployer import MLflowModelDeployer
-# from zenml.integrations.mlflow.services import MLflowDeploymentService
+
 from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
 
 from steps.clean_data import clean_df
@@ -18,7 +16,7 @@ from steps.eval_model import eval_model
 
 docker_settings = DockerSettings(required_integration=[MLFLOW])
 
-class Deployment_trigger_config(BaseParameters):
+class Deployment_trigger_config(BaseModel):
     """
     Parameters for deployment trigger.
     """
@@ -31,7 +29,7 @@ def trigger_deployment( accuracy : float , config: Deployment_trigger_config) ->
     """
     return accuracy >= config.min_accuracy
 
-@pipeline(enable_cache=True, settings={"docker_settings": docker_settings})
+@pipeline(enable_cache=False, settings={"docker": docker_settings})
 def continous_deployement_pipeline(data_path : str,
                                    workers : int = 1,
                                    timeout : int = DEFAULT_SERVICE_START_STOP_TIMEOUT
@@ -47,6 +45,6 @@ def continous_deployement_pipeline(data_path : str,
     deployment_decision = trigger_deployment(accuracy=r2)
 
     mlflow_model_deployer_step(model=model,
-                               deployment_decision=deployment_decision,
+                               deploy_decision=deployment_decision,
                                workers=workers,
                                timeout=timeout)
