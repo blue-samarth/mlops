c13ddaabee3956e4af22d582f619384cad70aeac
blue-samarth <samarth38work@gmail.com>
2025-01-18 01:03:35 +0530

    ci pipeline v1

--- run_deployment.py
+++ run_deployment.py
@@ -1,3 +1,4 @@
+from typing import cast
 import click
 from rich import print
 
@@ -30,7 +31,52 @@ def run_deployment(data_path : str, config : str, min_accuracy : float):
     if deploy:
         print("Deploying model...")
         continous_deployement_pipeline(data_path=data_path,
+                                       min_accuracy=min_accuracy,
                                        workers=3,
                                        timeout=60)
 
         print("Model deployed successfully.")
+    if predict:
+        print("Predicting using the model...")
+        # mlflow_service = MLflowDeploymentService(tracking_uri=get_tracking_uri())
+        # mlflow_service.predict()
+        print("Prediction done.")
+    print(
+        "You can now run"
+        f"[italic green]mlflow ui --backend-store-uri {get_tracking_uri()}[/italic green]"
+        "\nto see the model in the MLflow UI.\n"
+        "We can find our tracked runs within the `mlflow_pipeline` experiment."
+    )
+
+    existing_services = mlflow_model_deployer_component.find_model_server(
+        pipeline_name="continous_deployement_pipeline",
+        pipeline_step_name="mlflow_model_deployer_step",
+        model_name="model",
+    )
+
+    if existing_services:
+        service : MLflowDeploymentService = cast(MLflowDeploymentService, existing_services[0])
+        print("Model server found.")
+        if service.is_running():
+            print(
+                "The MLFLOW prediction service is running as daemon"
+                "process services and accepts interface requests at"
+                f"{service.prediction_uri}"
+                "To stop the service, run"
+                f"[italic green]`zenml model-deployer model delete`[/italic green]"
+                f"{str(service.uuid)}"
+            )
+        elif service.is_failed():
+            print("The service failed to start."
+                  f"last state: {service.status.state.value}"
+                  f"last error: {service.status.last_error}"
+                  )
+            
+        else:
+            print(
+                "The MLFLOW prediction service is not running."
+            )
+
+if __name__ == "__main__":
+    data_path = "data/olist_customers_dataset.csv"
+    run_deployment(data_path=data_path)
