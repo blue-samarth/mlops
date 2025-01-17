044195b01d82c6cc274c799b5b368abe8bf79f7c
blue-samarth <samarth38work@gmail.com>
2025-01-18 03:02:30 +0530

    dependencies issues

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 from zenml import pipeline , step  
-from zenml.steps import BaseParameters #, Output
+from zenml.steps.base_steps import BaseParameters #, Output
 from zenml.config import DockerSettings
 from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
 from zenml.integrations.constants import MLFLOW
--- run_deployment.py
+++ run_deployment.py
@@ -2,9 +2,9 @@ from typing import cast
 import click
 from rich import print
 
-from zenml.integrations.mlflow_utils import get_tracking_uri
-from zenml.integrations.mlflow.model_deployer import MLflowModelDeployer
-from zenml.integrations.mlflow.services import MLflowDeploymentService
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import MLFlowModelDeployer
+from zenml.integrations.mlflow.services.mlflow_deployment import MLFlowDeploymentService
 
 from pipelines.deployment_pipeline import continous_deployement_pipeline
 
@@ -24,7 +24,7 @@ DEPLOY_AND_PREDICT : str = "deploy_and_predict"
 @click.option("--min-accuracy", type=float, default=0.92, help="Minimum accuracy for deployment.")
 
 def run_deployment(data_path : str, config : str, min_accuracy : float):
-    mlflow_model_deployer_component = MLflowModelDeployer.get_active_model_deployer()
+    mlflow_model_deployer_component = MLFlowModelDeployer.get_active_model_deployer()
     deploy = config == DEPLOY or config == DEPLOY_AND_PREDICT
     predict = config == PREDICT or config == DEPLOY_AND_PREDICT
 
